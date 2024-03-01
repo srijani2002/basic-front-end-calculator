b474d1844f2f8e9abae8ab5ac684ccb140d97cfe
srijani2002 <151017602+srijani2002@users.noreply.github.com>
2024-03-01 20:30:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,181 @@
+var eqn="";
+document.querySelectorAll(".btn")[0].addEventListener("click", function(){
+    document.querySelector(".btn").classList.add("click");
+    setTimeout(() => {
+        document.querySelector(".btn").classList.remove("click");
+    }, 250);
+    eqn=eqn+"7";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[1].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[1].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[1].classList.remove("click");
+    }, 250);
+    eqn=eqn+"8";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[2].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[2].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[2].classList.remove("click");
+    }, 250);
+    eqn=eqn+"9";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[3].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[3].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[3].classList.remove("click");
+    }, 250);
+    eqn=eqn+"/";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[4].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[4].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[4].classList.remove("click");
+    }, 250);
+    eqn=eqn+"%";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[5].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[5].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[5].classList.remove("click");
+    }, 250);
+    eqn=eqn+"4";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[6].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[6].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[6].classList.remove("click");
+    }, 250);
+    eqn=eqn+"5";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[7].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[7].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[7].classList.remove("click");
+    }, 250);
+    eqn=eqn+"6";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[8].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[8].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[8].classList.remove("click");
+    }, 250);
+    eqn=eqn+"*";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[9].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[9].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[9].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[9].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[10].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[10].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[10].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[10].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[11].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[11].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[11].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[11].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[12].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[12].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[12].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[12].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[13].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[13].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[13].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[13].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[14].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[14].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[14].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[14].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[15].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[15].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[15].classList.remove("click");
+    }, 250);
+    eqn="";
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[16].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[16].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[16].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[16].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[17].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[17].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[17].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[17].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[18].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[18].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[18].classList.remove("click");
+    }, 250);
+    eqn=eqn+document.querySelectorAll(".btn")[18].innerHTML;
+    document.querySelector(".display").innerHTML=eqn;
+});
+
+document.querySelectorAll(".btn")[19].addEventListener("click", function(){
+    document.querySelectorAll(".btn")[19].classList.add("click");
+    setTimeout(() => {
+        document.querySelectorAll(".btn")[19].classList.remove("click");
+    }, 250);
+    eqn=eval(eqn);
+    document.querySelector(".display").innerHTML=eqn;
+});
+
